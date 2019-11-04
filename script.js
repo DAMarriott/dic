caf4628f4f087f999c87b14ce6d2a33944035909
DAMarriott <43251422+DAMarriott@users.noreply.github.com>
2019-11-04 11:42:24 -0800

    update script.js

--- script.js
+++ script.js
@@ -4,58 +4,83 @@ const apiKey = "18fe046869afc29a3c7155152d1a5943e64ff6e0";
 
 const searchURL = "https://owlbot.info/api/v3/dictionary/";
 
-
 function displayResults(responseJson) {
   console.log(responseJson);
 
   $("#results-list").empty();
-  $("#results-list").append(
-    `<div><h3>${responseJson.word}</h3>`
-  )
+  $("#results-list").append(`<h3>${responseJson.word}</h3>`);
   for (let i = 0; i < responseJson.definitions.length; i++) {
     $("#results-list").append(
-      `
-      <p>${i+1}. ${responseJson.definitions[i].type}</p>
-      <p>${responseJson.pronunciation}</p>
-      <p>${responseJson.definitions[i].definition}</p>
+      `<div class="resultbox">
+      <p>${i + 1}. ${responseJson.definitions[i].type}</p>
+      <p>Pronunciation: ${responseJson.pronunciation}</p>
+      <p>Definition: ${responseJson.definitions[i].definition}</p>
       </div>`
     );
   }
 
   $("#results").removeClass("hidden");
+
+  let newHistoryItem = $(
+    `<li><a href="#${responseJson.word}">${responseJson.word}</a></li>`
+  );
+  newHistoryItem.click(function() {
+    performSearch(responseJson.word);
+  });
+  $("#history-list").append(newHistoryItem);
+  $("#history").removeClass("hidden");
 }
 
 function getDefinition(query) {
   const url = searchURL + query;
   console.log(url);
-  fetch(url, {
-      headers: {
-          Authorization: `Token ${apiKey}`
-      }
-    }
-  )
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      else {
-        throw new Error(response.statusText);
-      }
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $("#js-error-message").text(`Something went wrong: ${err.message}`);
-    });
-}
 
-const searchTerm = $("#js-search-term").val();
+  fetch(url, {
+    headers: {
+      Authorization: `Token ${apiKey}`
+    }
+  })
+    .then(handleResponse)
+    .then(responseJson => displayResults(responseJson))
+    .catch(handleError);
+}
 
 function watchForm() {
   $("form").submit(event => {
     event.preventDefault();
     const searchTerm = $("#js-search-term").val();
-    getDefinition(searchTerm);
+    performSearch(searchTerm);
   });
 }
 
 $(watchForm);
+
+function performSearch(word) {
+  getDefinition(word);
+  $("#results-list").empty();
+  $("#js-error-message").empty();
+}
+
+const handleResponse = response => {
+  if (!response.ok) {
+    if (response.statusText) {
+      throw new Error(response.statusText);
+    } else {
+      throw response;
+    }
+  }
+
+  return response.json();
+};
+
+const handleError = err => {
+  if (typeof err.json === "function") {
+    err.json().then(errBody => {
+      $("#js-error-message").text(
+        `Something went wrong: ${errBody[0].message}`
+      );
+    });
+  } else {
+    $("#js-error-message").text(`Something went wrong: ${err.message}`);
+  }
+};
