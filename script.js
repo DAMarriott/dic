d96d333d3085dae122c099a9122da60ca1bb3437
DAMarriott <43251422+DAMarriott@users.noreply.github.com>
2020-01-07 13:42:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 import { getGif } from "./gif-script.js";
 import { performSearch } from "./definition-script.js";
+import { searchHistory, loadHistory } from "./searchHistory.js";
 
 function watchForm() {
   $("form").submit(event => {
     event.preventDefault();
     const searchTerm = $("#js-search-term").val();
-    performSearch(searchTerm);
-    getGif(searchTerm);
+    performSearch(searchTerm, () => {
+      getGif(searchTerm);
+      searchHistory(searchTerm);
+    });
+  });
+  $("button").click(event => {
+    localStorage.clear();
+    $("#history").empty();
   });
 }
 
 $(watchForm);
+$(loadHistory);
